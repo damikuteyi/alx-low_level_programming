8ab802b8d79faa257e98dc825dbd65388e8eddda
damikuteyi <kuteyidamilola@gmail.com>
2022-06-30 13:49:04 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  * This is the hello, printf task
- * Return: return 1 after program execution
- * */
+ * Return: return 1 after program execution 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
