24d766b6954fc5247121f7860b264e4eb884d6fd
damikuteyi <kuteyidamilola@gmail.com>
2022-07-04 15:39:27 -0700

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,15 @@
  *
  *Return - 0 for (success)
  */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
 
-int main(void) {
-
-char lower = 'a';
-char upper = 'A';
-
-for ( ; lower <= 'z'; lower++)
-putchar(lower);
-for ( ; upper <= 'Z'; upper++)
-putchar(upper);
-putchar(10);
-return (0);
-
+	for ( ; lower <= 'z'; lower++)
+		putchar(lower);
+	for ( ; upper <= 'Z'; upper++)
+		putchar(upper);
+	putchar(10);
+	return (0);
 }
