2fa97d8d19e0bbb5fcf691c8c4d0be21f91f0ecf
damikuteyi <kuteyidamilola@gmail.com>
2022-07-04 15:09:22 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet reversed using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'z';
+	for ( ; alpha >= 'a' ; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar(10);
+	return (0);
+}
