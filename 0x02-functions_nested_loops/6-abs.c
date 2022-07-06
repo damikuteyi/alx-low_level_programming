cb16115bdcf9d136e369152900581bdd469f8346
damikuteyi <kuteyidamilola@gmail.com>
2022-07-05 18:17:31 -0700

    6-main.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calculate absolute value for input
+ * @n : integer value
+ * Return: absolute value of n
+ *
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+return ((-1) * n);
+}
