ba7a891a9c6716091734192227b5a220f9ccee8a
damikuteyi <kuteyidamilola@gmail.com>
2022-07-12 18:30:24 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints out a string with newline
+ * @str: string which is to be printed
+ *
+ * Return: (void)
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putschar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
