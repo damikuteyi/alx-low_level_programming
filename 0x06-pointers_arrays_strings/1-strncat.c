950ddc1ac97f90b053af8c6f7db40a622538d209
damikuteyi <kuteyidamilola@gmail.com>
2022-07-15 05:12:50 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates string with n bytes from another string
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to be copied
+ * *Return: destination string concatenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0') /*i is the index of null terminator*/
+		i++;
+	while (j < n && src[j] != '\0') /*append replacing null termniator*/
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
