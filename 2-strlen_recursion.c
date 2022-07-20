d4164fc253933132c8fa6793e67adfb0f343cecc
damikuteyi <kuteyidamilola@gmail.com>
2022-07-20 16:58:53 -0700

    2-strlen_recursion.c

---
+++ 2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return string length
+ * @s: string
+ * Return: string length
+ */
+int_strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursions(s + 1));
+	}
+}
